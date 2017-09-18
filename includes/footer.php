431c957954090c4463217f944eecd3e453f5333f
Samuel <adeyemosamuel@gmail.com>
2017-09-18 01:13:18 +0100

    testing1

---
+++ includes/footer.php
@@ -0,0 +1,124 @@
+<div id="footer"><!-- footer Starts -->
+<div class="container"><!-- container Starts -->
+<div class="row"><!-- row Starts -->
+<div class="col-md-3 col-sm-6"><!-- col-md-3 col-sm-6 Starts -->
+
+<h4>Pages</h4>
+
+<ul><!-- ul Starts -->
+<li><a href="cart.php">Shopping Cart</a></li>
+
+<li><a href="contact.php">Contact Us</a></li>
+
+<li><a href="shop.php">Shop</a></li>
+
+<li><a href="checkout.php">My Account</a></li>
+
+</ul><!-- ul Ends -->
+
+<hr>
+
+<h4>User Section</h4>
+<ul><!-- ul Starts -->
+
+<li><a href="checkout.php">Login</a></li>
+<li><a href="customer_reg.php">Register</a></li>
+
+</ul><!-- ul Ends -->
+
+<hr class="hidden-md hidden-lg hidden-sm">
+</div><!-- col-md-3 col-sm-6 Ends -->
+<div class="col-md-3 col-sm-6"><!-- col-md-3 col-sm-6 Starts -->
+
+<h4> Top Products Categories </h4>
+<ul><!-- ul Starts -->
+
+<li><a href="#">Men Fashion</a></li>
+<li><a href="#">Women Fashion</a></li>
+<li><a href="#">Kids Fashion</a></li>
+<li><a href="#">Sunglasses</a></li>
+<li><a href="#">Watches</a></li>
+<li><a href="#">Fragrances</a></li>
+
+</ul><!-- ul Ends -->
+<hr class="hidden-md hidden-lg">
+
+</div><!-- col-md-3 col-sm-6 Ends -->
+<div class="col-md-3 col-sm-6"><!-- col-md-3 col-sm-6 Starts -->
+
+<h4>Where to find us</h4>
+<p><!-- p Starts -->
+<strong>Metro Online Services Limited</strong>
+<br>3A, CMD Road, Ikosi-Ketu
+<br>Lagos
+<br>08066738373, 08170087989
+<br>enquiries@metrostores.ng
+<br>
+<strong>Adeyemo Samuel</strong>
+
+</p><!-- p Ends -->
+
+<a id="contpage" href="contact.php"> Go to Contact us page</a>
+
+<hr class="hidden-md hidden-lg">
+
+</div><!-- col-md-3 col-sm-6 Ends -->
+
+<div class="col-md-3 col-sm-6"><!-- col-md-3 col-sm-6 Starts -->
+<h4>Get the news</h4>
+
+
+<p class="text-muted">
+Subscribe to our newsletter to get latest updates on all of our products from different categories and promotions.
+</p>
+<form action="" method="post"><!-- form Starts -->
+
+<div class="input-group"><!-- input-group Starts -->
+
+<input type="text" class="form-control" name="email">
+<span class="input-group-btn"><!-- input-group-btn Starts -->
+<input type="submit" value="subscribe" class="btn btn-danger">
+
+</span><!-- input-group-btn Ends -->
+</div><!-- input-group Starts -->
+
+</form><!-- form Ends -->
+
+<hr>
+<h4>Stay in touch</h4>
+
+<p class="social"><!-- social Starts -->
+
+<a id="fbk" href="#"><i class="fa fa-facebook"></i></a>
+<a id="twt" href="#"><i class="fa fa-twitter"></i></a>
+<a id="inst" href="#"><i class="fa fa-instagram"></i></a>
+<a id="google" href="#"><i class="fa fa-google-plus"></i></a>
+<a id="email" href="#"><i class="fa fa-envelope"></i></a>
+
+</p><!-- social Ends -->
+
+</div><!-- col-md-3 col-sm-6 Ends -->
+
+</div><!-- row Ends -->
+</div><!-- container Ends -->
+</div><!-- footer Ends -->
+
+<div id="copyright"><!-- copyright Starts -->
+<div class="container"><!-- container Starts -->
+<div class="col-md-6"><!-- col-md-6 Starts -->
+<p class="pull-left"> &copy; 2017 Metro Online Services Limited </p>
+
+
+
+</div><!-- col-md-6 Ends -->
+<div class="col-md-6"><!-- col-md-6 Starts -->
+
+<p class="pull-right">
+Web Design by <a href="https://www.facebook.com/olumightys">Fixit Technologies</a>
+
+</p>
+
+</div><!-- col-md-6 Ends -->
+
+</div><!-- container Ends -->
+</div><!-- copyright Ends -->
---
+++ index.php
@@ -0,0 +1,498 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>MetrostoresNG</title>
+<link href="http://fonts.googleapis.com/css?family=Roboto:400,500,700,300,100" rel="stylesheet">
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/js/bootstrap.min.js"></script> -->
+<link href="styles/bootstrap.min.css" rel="stylesheet">
+
+<link href="styles/style.css" rel="stylesheet">
+
+<link href="font_awesome/css/font-awesome.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+<div  id="top"><!-- top Starts -->
+
+  <div class="container"><!--container Starts -->
+
+    <div class="col-md-6 offer"><!-- col-m6 offer Starts -->
+
+<a href="#" class="btn btn-danger btn-sm">
+Welcome: Guest
+</a>
+
+<a href="#">
+Shopping Cart Total Price: ₦100, Total Items 2
+</a>
+
+    </div><!-- col-m6 offer Ends -->
+
+
+<div class="col-md-6"><!-- col-md-6 Starts -->
+<ul class="menu"><!-- menu Starts -->
+
+<li>
+  <a href="customer_register.php">
+    Register
+  </a>
+</li>
+
+<li>
+<a href="checkout.php">
+  My Account
+</a>
+</li>
+
+
+<li>
+  <a href="cart.php">
+    Go to Cart
+</a>
+</li>
+
+
+<li>
+<a href="checkout.php">
+Login
+</a>
+</li>
+
+
+<li>
+<a href="sellerhq.php">
+Become a Seller
+</a>
+</li>
+
+
+</ul><!-- menu Ends -->
+
+
+</div><!-- col-md-6 Ends -->
+
+
+
+  </div><!--container Ends -->
+
+</div><!-- top Ends -->
+
+<div class="navbar navbar-default" id="navbar"><!-- navbar navbar-default Starts -->
+<div class="container"><!-- container Starts -->
+<div class="navbar-header"><!-- navbar-header Starts -->
+<a class="navbar-brand home" href="index.php"><!-- navbar navbar-brand home Starts -->
+
+<img src="images/rsz_metro2.png" alt="MetrostoresNG logo" class="hidden-xs">
+<img src="images/rsz_metro2.png" alt="MetrostoresNG logo" class="visible-xs">
+
+
+
+
+</a><!-- navbar navbar-brand home Ends -->
+
+<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation">
+<span class="sr-only">Toggle Navigation</span>
+<i class="fa fa-align-justify"></i>
+</button>
+
+<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#search">
+<span class="sr-only">Toggle Search</span>
+<i class="fa fa-search"></i>
+</button>
+
+
+</div><!-- navbar-header Ends -->
+
+<div class="navbar-collapse collapse" id="navigation"><!-- navbar-collapse Starts -->
+
+<div class="padding-nav"><!-- padding-nav Starts -->
+
+<ul class="nav navbar-nav navbar-left"><!-- nav navbar-nav navbar-left Starts -->
+<li>
+  <a href="index.php"> Home </a>
+</li>
+
+<li>
+  <a href="shop.php"> Shop </a>
+</li>
+
+<li>
+  <a href="checkout.php"> My Account </a>
+</li>
+
+<li>
+  <a href="cart.php"> Shopping Cart </a>
+</li>
+
+<li>
+  <a href="contact.php"> Contact Us </a>
+</li>
+
+</ul><!-- nav navbar-nav navbar-left Ends -->
+
+</div><!-- padding-nav Ends -->
+<a class="btn btn-primary navbar-btn right" href="cart.php"><!-- btn btn-primary navbar-btn right Starts -->
+
+<i class="fa fa-shopping-cart"></i>
+<span> 4 items in cart </span>
+</a><!-- btn btn-primary navbar-btn right Ends -->
+
+<div class="navbar-collapse collapse right"><!-- navbar-collapse collapse right Starts -->
+
+<button class="btn navbar-btn btn-primary" type="button" data-toggle="collapse" data-target="#search">
+
+<span class="sr-only">Toggle Search</span>
+<i class="fa fa-search"></i>
+
+</button>
+
+</div><!-- navbar-collapse collapse right Ends -->
+
+<div class="collapse clearfix" id="search"><!-- collapse clearfix Starts -->
+
+<form class="navbar-form" method="get" action="results.php"><!-- navbar-form Starts -->
+<div class="input-group"><!-- input-group Starts -->
+<input class="form-control" type="text" placeholder="Search" name="user_query" required>
+<span class="input-group-btn"><!-- input-group-btn Starts -->
+
+<button type="submit" value="Search" name="search" class="btn btn-primary">
+
+<i class="fa fa-search"></i>
+
+</button>
+
+</span><!-- input-group-btn Ends -->
+
+</div><!-- input-group Ends -->
+
+</form><!-- navbar-form Ends -->
+</div><!-- collapse clearfix Ends -->
+
+
+</div><!-- navbar-collapse Ends -->
+
+</div><!-- container Ends -->
+</div><!-- navbar navbar-default Ends -->
+
+<div claas="container" id="slider"><!-- container Starts -->
+
+<div class="col-md-12"><!-- col-md-12 Starts -->
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel"><!-- carousel slide Starts -->
+<ol class="carousel-indicators"><!-- carousel-indicators Starts -->
+
+<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+
+<li data-target="#myCarousel" data-slide-to="1"></li>
+
+<li data-target="#myCarousel" data-slide-to="2"></li>
+
+<li data-target="#myCarousel" data-slide-to="3"></li>
+
+</ol><!-- carousel-indicators Ends -->
+
+<div class="carousel-inner"><!-- carousel-inner Starts -->
+<div class="item active">
+<img src="admin_area/slides_images/a1.jpg">
+</div>
+
+<div class="item">
+<img src="admin_area/slides_images/b1.jpg">
+</div>
+
+<div class="item">
+<img src="admin_area/slides_images/c1.jpg">
+</div>
+
+<div class="item">
+<img src="admin_area/slides_images/d1.jpg">
+</div>
+
+</div><!-- carousel-inner Ends -->
+
+<a class="left carousel-control" href="#myCarousel" data-slide="prev"><!-- left carousel-control Starts -->
+
+<span class="glyphicon glyphicon-chevron-left"></span>
+<span class="sr-only"> Previous </span>
+
+</a><!-- left carousel-control Ends -->
+
+<a class="right carousel-control" href="#myCarousel" data-slide="next"><!-- right carousel-control Starts -->
+<span class="glyphicon glyphicon-chevron-right"></span>
+
+<span class="sr-only"> Next </span>
+</a><!-- right carousel-control Ends -->
+
+</div><!-- carousel slide Ends -->
+
+</div><!-- col-md-12 Ends -->
+</div><!-- container Ends -->
+
+<div id="advantages"><!-- advantages Starts -->
+<div class="container" style="margin-top:600px;"><!-- container Starts -->   
+<div class="same-height-row"><!-- same-height-row Starts-->
+
+<div class="col-sm-4"><!-- col-sm-4 Starts -->
+
+<div class="box same-height"><!-- box same-height Starts -->
+<div class="icon">
+<i class="fa fa-heart"></i>
+</div>
+
+<h3><a href="#"> WE LOVE OUR CUSTOMERS </a></h3>
+<p>
+We are known to provide best possible service ever
+</p>
+</div><!-- box same-height Ends -->
+
+</div><!-- col-sm-4 Ends -->
+
+
+
+<div class="col-sm-4"><!-- col-sm-4 Starts -->
+
+<div class="box same-height"><!-- box same-height Starts -->
+<div class="icon">
+<i class="fa fa-tags"></i>
+</div>
+
+<h3><a href="#"> BEST PRICES </a></h3>
+<p>
+You can check on all other sites about the prices and then compare with us 
+</p>
+</div><!-- box same-height Ends -->
+
+</div><!-- col-sm-4 Ends -->
+
+
+<div class="col-sm-4"><!-- col-sm-4 Starts -->
+
+<div class="box same-height"><!-- box same-height Starts -->
+<div class="icon">
+<i class="fa fa-thumbs-up"></i>
+</div>
+
+<h3><a href="#"> 100% SATISFACTION GUARANTEED </a></h3>
+<p>
+Free returns on everything for 3 months
+</p>
+</div><!-- box same-height Ends -->
+
+</div><!-- col-sm-4 Ends -->
+
+
+</div><!-- same-height-row Ends -->
+</div><!-- container Ends -->
+</div><!-- advantages Ends -->
+
+<div id="hot"><!-- hot Starts -->
+<div class="box"><!-- box Starts -->
+
+<div class="container"><!-- container Starts -->
+<div class="col-md-12"><!-- col-md-12 Starts -->
+
+<h2> Latest this week </h2>
+
+</div><!-- col-md-12 Ends -->
+</div><!-- container Ends -->
+</div><!-- Box Ends -->
+</div><!-- hot Ends -->
+
+<div id="content" class="container"><!-- container Starts -->
+<div class="row"><!-- row Starts -->
+
+<div class="col-sm-4 col-sm-6 single"><!-- col-sm-4 col-sm-6 single Starts -->
+
+<div class="product"><!-- product Starts -->
+<a href="details.php">
+<img src="admin_area/product_images/product.jpg" class"img-responsive">
+</a>
+
+<div class="text"><!-- text Starts -->
+<h3><a href="details.php">Men's Casual Long-Sleeved Shirt</a></h3>
+<p class="price">₦4500</p>
+<p class="buttons">
+<a href="details.php" class="btn btn-default">View Details</a>
+<a href="details.php" class="btn btn-primary">
+<i class="fa fa-shopping-cart"></i> Add to Cart
+
+</a>
+</p>
+</div><!-- text Ends -->
+
+</div><!-- product Ends -->
+</div><!-- col-sm-4 col-sm-6 single Ends -->
+
+<div class="col-sm-4 col-sm-6 single"><!-- col-sm-4 col-sm-6 single Starts -->
+
+<div class="product"><!-- product Starts -->
+<a href="details.php">
+<img src="admin_area/product_images/product.jpg" class"img-responsive">
+</a>
+
+<div class="text"><!-- text Starts -->
+<h3><a href="details.php">Men's Casual Long-Sleeved Shirt</a></h3>
+<p class="price">₦4500</p>
+<p class="buttons">
+<a href="details.php" class="btn btn-default">View Details</a>
+<a href="details.php" class="btn btn-primary">
+<i class="fa fa-shopping-cart"></i> Add to Cart
+
+</a>
+</p>
+</div><!-- text Ends -->
+
+</div><!-- product Ends -->
+</div><!-- col-sm-4 col-sm-6 single Ends -->
+
+<div class="col-sm-4 col-sm-6 single"><!-- col-sm-4 col-sm-6 single Starts -->
+
+<div class="product"><!-- product Starts -->
+<a href="details.php">
+<img src="admin_area/product_images/product.jpg" class"img-responsive">
+</a>
+
+<div class="text"><!-- text Starts -->
+<h3><a href="details.php">Men's Casual Long-Sleeved Shirt</a></h3>
+<p class="price">₦4500</p>
+<p class="buttons">
+<a href="details.php" class="btn btn-default">View Details</a>
+<a href="details.php" class="btn btn-primary">
+<i class="fa fa-shopping-cart"></i> Add to Cart
+
+</a>
+</p>
+</div><!-- text Ends -->
+
+</div><!-- product Ends -->
+</div><!-- col-sm-4 col-sm-6 single Ends -->
+
+<div class="col-sm-4 col-sm-6 single"><!-- col-sm-4 col-sm-6 single Starts -->
+
+<div class="product"><!-- product Starts -->
+<a href="details.php">
+<img src="admin_area/product_images/product.jpg" class"img-responsive">
+</a>
+
+<div class="text"><!-- text Starts -->
+<h3><a href="details.php">Men's Casual Long-Sleeved Shirt</a></h3>
+<p class="price">₦4500</p>
+<p class="buttons">
+<a href="details.php" class="btn btn-default">View Details</a>
+<a href="details.php" class="btn btn-primary">
+<i class="fa fa-shopping-cart"></i> Add to Cart
+
+</a>
+</p>
+</div><!-- text Ends -->
+
+</div><!-- product Ends -->
+</div><!-- col-sm-4 col-sm-6 single Ends -->
+
+<div class="col-sm-4 col-sm-6 single"><!-- col-sm-4 col-sm-6 single Starts -->
+
+<div class="product"><!-- product Starts -->
+<a href="details.php">
+<img src="admin_area/product_images/product.jpg" class"img-responsive">
+</a>
+
+<div class="text"><!-- text Starts -->
+<h3><a href="details.php">Men's Casual Long-Sleeved Shirt</a></h3>
+<p class="price">₦4500</p>
+<p class="buttons">
+<a href="details.php" class="btn btn-default">View Details</a>
+<a href="details.php" class="btn btn-primary">
+<i class="fa fa-shopping-cart"></i> Add to Cart
+
+</a>
+</p>
+</div><!-- text Ends -->
+
+</div><!-- product Ends -->
+</div><!-- col-sm-4 col-sm-6 single Ends -->
+
+<div class="col-sm-4 col-sm-6 single"><!-- col-sm-4 col-sm-6 single Starts -->
+
+<div class="product"><!-- product Starts -->
+<a href="details.php">
+<img src="admin_area/product_images/product.jpg" class"img-responsive">
+</a>
+
+<div class="text"><!-- text Starts -->
+<h3><a href="details.php">Men's Casual Long-Sleeved Shirt</a></h3>
+<p class="price">₦4500</p>
+<p class="buttons">
+<a href="details.php" class="btn btn-default">View Details</a>
+<a href="details.php" class="btn btn-primary">
+<i class="fa fa-shopping-cart"></i> Add to Cart
+
+</a>
+</p>
+</div><!-- text Ends -->
+
+</div><!-- product Ends -->
+</div><!-- col-sm-4 col-sm-6 single Ends -->
+
+
+<div class="col-sm-4 col-sm-6 single"><!-- col-sm-4 col-sm-6 single Starts -->
+
+<div class="product"><!-- product Starts -->
+<a href="details.php">
+<img src="admin_area/product_images/product.jpg" class"img-responsive">
+</a>
+
+<div class="text"><!-- text Starts -->
+<h3><a href="details.php">Men's Casual Long-Sleeved Shirt</a></h3>
+<p class="price">₦4500</p>
+<p class="buttons">
+<a href="details.php" class="btn btn-default">View Details</a>
+<a href="details.php" class="btn btn-primary">
+<i class="fa fa-shopping-cart"></i> Add to Cart
+
+</a>
+</p>
+</div><!-- text Ends -->
+
+</div><!-- product Ends -->
+</div><!-- col-sm-4 col-sm-6 single Ends -->
+
+
+<div class="col-sm-4 col-sm-6 single"><!-- col-sm-4 col-sm-6 single Starts -->
+
+<div class="product"><!-- product Starts -->
+<a href="details.php">
+<img src="admin_area/product_images/product.jpg" class"img-responsive">
+</a>
+
+<div class="text"><!-- text Starts -->
+<h3><a href="details.php">Men's Casual Long-Sleeved Shirt</a></h3>
+<p class="price">₦4500</p>
+<p class="buttons">
+<a href="details.php" class="btn btn-default">View Details</a>
+<a href="details.php" class="btn btn-primary">
+<i class="fa fa-shopping-cart"></i> Add to Cart
+
+</a>
+</p>
+</div><!-- text Ends -->
+
+</div><!-- product Ends -->
+</div><!-- col-sm-4 col-sm-6 single Ends -->
+</div><!-- row Ends -->
+</div><!-- container Ends -->
+
+<?php
+include("includes/footer.php");
+
+
+
+
+?>
+<script src="js/jquery-3.2.1.min.js"></script>
+
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
